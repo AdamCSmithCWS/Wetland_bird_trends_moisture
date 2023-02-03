515135932c0d784a3bb07d29bcda9f4104ed5ef2
AdamCSmithCWS <adamcsmith.birdstats@gmail.com>
2023-02-03 15:45:59 -0500

    adding basic base model with bbsBayes2

--- scripts_modeling/BaseModel_run_no_covariates.R
+++ scripts_modeling/BaseModel_run_no_covariates.R
@@ -1,14 +1,37 @@
-### runing bbsBayes spatial GAMYE model with spatial
-### year-effects (not just random)
+### running bbsBayes spatial GAMYE model
 
 
-library(bbsBayes)
+
+library(bbsBayes2)
+
+species <- "Black Tern"
+stratification <- "latlong"
+
+model = "gamye"
+
+model_variant <- "spatial"
 
 
-bbsBayes::copy_model_file(model = "gamye",
-                          model_variant = "spatial",
-                          "models")
+s <- stratify(by = stratification,
+              species = species)
 
+
+p <- prepare_data(s)
+
+ps <- prepare_spatial(p,
+                      strata_map = load_map(stratification))
+
+print(ps$spatial_data$map)
+
+pm <- prepare_model(ps,
+                    model = model,
+                    model_variant = model_variant)
+
+fit <- run_model(pm,
+                 refresh = 200,
+                 #adapt_delta = 0.8,
+                 output_dir = "output",
+                 output_basename = paste(species,stratification,model,model_variant,sep = "_"))
 
 
 
